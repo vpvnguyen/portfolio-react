36466217b2b10b5cdb395282af6cae074b02d4d1
vpvnguyen <vpvnguyen@gmail.com>
2020-01-11 16:25:57 -0800

    PASS - useEffect, get('/')

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
   const [projects, setProjects] = useState('projects')
+
+  useEffect(() => {
+    console.log('useEffect()')
+    axios.get('http://ec2-13-59-49-63.us-east-2.compute.amazonaws.com/')
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  })
 
   return (
     <div className="App">
