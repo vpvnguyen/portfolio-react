eb8b28a09c056abc766025151d5d24918c1089ad
vpvnguyen <vpvnguyen@gmail.com>
2020-01-17 21:06:46 -0800

    render menu component

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from 'axios';
+
+// components
+import Menu from './components/Menu/Menu.jsx';
 
 function App() {
-  const [projects, setProjects] = useState('projects')
-
-  useEffect(() => {
-    console.log('useEffect()')
-    axios.get('http://ec2-13-59-49-63.us-east-2.compute.amazonaws.com/')
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-  })
 
   return (
     <div className="App">
-      front page
+      <Menu />
     </div>
   );
 }
