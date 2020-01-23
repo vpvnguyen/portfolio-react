0ffc021662cadf7be92624f462fceba8cdbca230
vpvnguyen <vpvnguyen@gmail.com>
2020-01-22 22:26:49 -0800

    created Projects API model

---
+++ app/src/components/Projects/All/All.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// get all projects from mongoDB/portfolio
+export default function All() {
+
+    // 
+    return (
+        <div>
+            test
+        </div>
+    )
+}
