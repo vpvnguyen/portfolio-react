90639ee50e2d1fc812e4251adf14a643fa785d69
vpvnguyen <vpvnguyen@gmail.com>
2020-01-23 18:41:27 -0800

    import material-ui form

--- app/src/utils/Projects.js
+++ app/src/utils/Projects.js
@@ -1,24 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
+// define actions to mongodb
 export const Projects = {
-    // get all projects
-    getAll: () => {
-        return axios.get('http://localhost:3001/api/projects')
-    },
-    // upload a project
-    upload: () => {
-        return axios.post('http://localhost:3001/api/projects/create');
-    },
-    // update a project
-    update: () => {
-        console.log('update project by id in mongodb/portfolio');
-    },
-    // delete a project
-    delete: (id) => {
-        return axios.delete(`http://localhost:3001/api/projects/delete/${id}`);
-    },
-    callBackend: () => {
-        return axios.get('http://localhost:3001/');
-    },
+  // get all projects
+  getAll: () => {
+    return axios.get("http://localhost:3001/api/projects");
+  },
+  // upload a project
+  upload: () => {
+    return axios.post("http://localhost:3001/api/projects/create");
+  },
+  // update a project by id;
+  update: (id, title, desc, link) => {
+    //axios.put(url[, data[, config]])
+    const updateObject = {
+      title: "potter",
+      desc: "new desc",
+      link: "newlinkurl.com"
+    };
 
+    const allKeys = Object.keys(updateObject);
+
+    console.log(allKeys)
+
+      return axios.put(`http://localhost:3001/api/projects/update/${id}`, 
+        updateObject
+      );
+  },
+  // delete a project
+  delete: id => {
+    return axios.delete(`http://localhost:3001/api/projects/delete/${id}`);
+  },
+  callBackend: () => {
+    return axios.get("http://localhost:3001/");
+  }
 };
