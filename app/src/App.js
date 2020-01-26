c5bdf5135191c25034d7286354677e4cae2653f2
vpvnguyen <vpvnguyen@gmail.com>
2020-01-26 12:37:58 -0800

    removed menu component; add login and sidebar components

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { useStyles } from './AppStyle';
 
-// components
-import Menu from './components/Menu/Menu.jsx';
+import Login from './components/Login/Login';
+import SideMenu from './components/SideMenu/SideMenu';
 
-function App() {
+export default function App() {
+  const classes = useStyles();
 
   return (
-    <div className="App">
-      <Menu />
+    <div className={classes.root}>
+    <CssBaseline />
+    <SideMenu />
+
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+            {/* Route between screens */}
+            <Login />
+
+      </main>
     </div>
   );
 }
-
-export default App;
